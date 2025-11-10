bd75764f5442a7bac7cbfd1d4c73586243bf3e0f
Zion Eloheeka <m.eloheeka12@gmail.com>
2025-11-10 21:59:15 +0200

    First Commint

---
+++ index.ts
@@ -0,0 +1,15 @@
+let userName: String = "Zion";
+let userAge: Number = 16;
+let userLocation: String = "Kigali,Rwanda";
+let  mentor : Boolean = false;
+
+let userName2: String = "Michael";
+let userAge2: Number = 22;
+let userLocation2: String = "Norhtern province";
+let  mentor2 : Boolean = true;
+
+console.log(`Welcome to skillSync ${userName}`);
+console.log(` Age : ${userAge} years old`);
+console.log(`Location :${userLocation}`);
+console.log(`Role : ${mentor ? "Mentor" :"Learner"}`)
+console.log(`${userName2} is ${mentor2 ? "a Mentor" : "a Learner"}`);
