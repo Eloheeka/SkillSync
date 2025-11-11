ec56fff3eb832b0193f845bc813381f74b4df8a3
Zion Eloheeka <m.eloheeka12@gmail.com>
2025-11-11 07:57:20 +0200

    New changes

--- index.ts
+++ index.ts
@@ -1,15 +1,53 @@
-let userName: String = "Zion";
-let userAge: Number = 16;
-let userLocation: String = "Kigali,Rwanda";
-let  mentor : Boolean = false;
 
-let userName2: String = "Michael";
-let userAge2: Number = 22;
-let userLocation2: String = "Norhtern province";
-let  mentor2 : Boolean = true;
 
-console.log(`Welcome to skillSync ${userName}`);
-console.log(` Age : ${userAge} years old`);
-console.log(`Location :${userLocation}`);
-console.log(`Role : ${mentor ? "Mentor" :"Learner"}`)
-console.log(`${userName2} is ${mentor2 ? "a Mentor" : "a Learner"}`);
+interface User{
+    name :String;
+    age : number;
+    location : String;
+    mentor : Boolean;
+}
+
+let userOne : User = {
+    name : "Eloheeka",
+    age : 17,
+    location: "Nyabihu",
+    mentor : false
+
+}
+
+let userTwo : User = {
+    name : "Kelly",
+    age : 20,
+    location: "Kigali,Rwanda",
+    mentor : true
+
+}
+
+let userThree : User = {
+    name : "Zion",
+    age : 30,
+    location: "NewYork,USA",
+    mentor : true
+
+}
+
+let userFour : User = {
+    name : "Manzi",
+    age : 188,
+    location: "Toronto,Canada",
+    mentor : false
+
+}
+
+const users : User[] = [userOne, userTwo, userThree];
+
+for(let user of users){
+    console.log(`${user.name} is from ${user.location} and is a ${user.mentor ? 'mentor' : 'learner'}`)
+}
+
+
+const mentors = users.filter(user => user.mentor === true);
+console.log(`Number of mentors : ${mentors.length}`);
+
+const rwandans = users.filter(user => user.location.includes("Rwanda"));
+console.log(`Number of Rwandans : ${rwandans.length}`);
