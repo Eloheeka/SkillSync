93493a7aab162302e417a56a7b5649052a263fa8
Zion Eloheeka <m.eloheeka12@gmail.com>
2025-11-11 21:50:43 +0200

    added some things

--- index.ts
+++ index.ts
@@ -51,3 +51,28 @@ console.log(`Number of mentors : ${mentors.length}`);
 
 const rwandans = users.filter(user => user.location.includes("Rwanda"));
 console.log(`Number of Rwandans : ${rwandans.length}`);
+
+function introduceUser(user: string, age? : number, location :string = "Kigali"){
+    console.log(age ? `${user} is ${age} and lives in ${location}` : `${user} prefers to keep age a secret. and lives in ${location}`)
+}
+
+console.log(introduceUser("Eloheeka",17,"Nyabihu"));
+console.log(introduceUser("Manzi",undefined,"Toronto"   ));
+
+let calc: (x:number, y:number) => number;
+ calc = (a,b) => a+b;
+ console.log(calc(5,10));
+
+ function format(value:string) :string;
+ function format(value:number) :number;
+ function format(value: number | string) : number | string {
+    return value.toString();
+ }
+
+ console.log(format("Eloheeka"));
+ console.log(format(12345));
+
+ let converter : (x: number) => string;
+ converter =  (num) => `Value: ${num}`;
+ console.log(converter(100));
+
