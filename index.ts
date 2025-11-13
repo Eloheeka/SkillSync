734172e77b92107185df74caa7e2ea74ae1cd058
Zion Eloheeka <m.eloheeka12@gmail.com>
2025-11-13 21:50:00 +0200

    UserManagement

--- index.ts
+++ index.ts
@@ -1,78 +1,228 @@
 
 
-interface User{
-    name :String;
-    age : number;
-    location : String;
-    mentor : Boolean;
+// interface User{
+//     name :String;
+//     age : number;
+//     location : String;
+//     mentor : Boolean;
+// }
+
+// let userOne : User = {
+//     name : "Eloheeka",
+//     age : 17,
+//     location: "Nyabihu",
+//     mentor : false
+
+// }
+
+// let userTwo : User = {
+//     name : "Kelly",
+//     age : 20,
+//     location: "Kigali,Rwanda",
+//     mentor : true
+
+// }
+
+// let userThree : User = {
+//     name : "Zion",
+//     age : 30,
+//     location: "NewYork,USA",
+//     mentor : true
+
+// }
+
+// let userFour : User = {
+//     name : "Manzi",
+//     age : 188,
+//     location: "Toronto,Canada",
+//     mentor : false
+
+// }
+
+// const users : User[] = [userOne, userTwo, userThree];
+
+// for(let user of users){
+//     console.log(`${user.name} is from ${user.location} and is a ${user.mentor ? 'mentor' : 'learner'}`)
+// }
+
+
+// const mentors = users.filter(user => user.mentor === true);
+// console.log(`Number of mentors : ${mentors.length}`);
+
+// const rwandans = users.filter(user => user.location.includes("Rwanda"));
+// console.log(`Number of Rwandans : ${rwandans.length}`);
+
+// function introduceUser(user: string, age? : number, location :string = "Kigali"){
+//     console.log(age ? `${user} is ${age} and lives in ${location}` : `${user} prefers to keep age a secret. and lives in ${location}`)
+// }
+
+// console.log(introduceUser("Eloheeka",17,"Nyabihu"));
+// console.log(introduceUser("Manzi",undefined,"Toronto"   ));
+
+// let calc: (x:number, y:number) => number;
+//  calc = (a,b) => a+b;
+//  console.log(calc(5,10));
+
+//  function format(value:string) :string;
+//  function format(value:number) :number;
+//  function format(value: number | string) : number | string {
+//     return value.toString();
+//  }
+
+//  console.log(format("Eloheeka"));
+//  console.log(format(12345));
+
+//  let converter : (x: number) => string;
+//  converter =  (num) => `Value: ${num}`;
+//  console.log(converter(100));
+
+//  interface person{
+//     name : string;
+//     age : number;
+//     intro() : void;
+//  }
+
+//  let individual : person ={
+//     name : "Zion",
+//     age:17,
+//     intro(){
+//         console.log(`Hi my name is ${individual.name} and I am ${individual.age} years old.`);
+//     }
+//  }
+
+//     individual.intro();
+
+//      let persona : person ={
+//     name : "Eloheeka",
+//     age:17,
+//     intro(){
+//       console.log(`person ${persona.name} does not talk about their age`);
+//     }
+//  }
+
+//  persona.intro();
+
+//  type ID = number | string;
+//  type Gender = "Male" | "Female" | "Other";
+
+//  interface profile {
+//     id:ID,
+//     name : string,
+//     gender: Gender
+//  }
+
+//  let person1 : profile = {
+//     id:1,
+//     name:"Kelly",
+//     gender: "Female"
+//  }
+
+//  console.log(`Name : ${person1.name} ID: ${person1.id} Gender: ${person1.gender}`);
+
+//  type Address = {city: string, country: string};
+//  type contact = { email: string, phone: string};
+
+//  type Details = Address & contact;
+
+//  const user: Details = {
+//     city: "Kigali",
+//     country: "Rwanda",
+//     email: "m.eloheeka12@gmail.com",
+//     phone: "+250788123456"
+//  }
+
+//  console.log(`City: ${user.city}, Country: ${user.country}, Email: ${user.email}, Phone: ${user.phone}`);
+
+
+//  interface Car{
+//     brand: string,
+//     model: string,
+//     year: number,
+//     start() : void;
+//  }
+
+//  type EletricCar = Car & {batteryCapacity:number, charge():void};
+
+//  let tesla :EletricCar = {
+//     brand: "Tesla",
+//     model: "Model S",
+//     year: 2022,
+//     batteryCapacity: 100,
+//     start(){
+//         console.log(`The ${this.brand} ${this.model} is starting.`);
+//     },
+//     charge() {
+//         console.log(`Charging the ${this.brand} ${this.model} with a capacity of ${this.batteryCapacity} kWh.`);
+//     },
+//  }
+
+//     tesla.start();
+//     tesla.charge();
+
+type Book = {
+    title: string,
+    author: string,
+    readonly isbn: number,
+    price: number;
+} 
+
+const MoreThanACrown : Book = {
+    title: "More Than A Crown",
+    author: "Nishimwe Naomie",
+    isbn: 9789963535220,
+    price: 20.5,
+};
+
+const{title, author} = MoreThanACrown;
+console.log(`Title: ${title}, Author: ${author}`);
+
+class Person {
+    name: string;
+    className: string;
+
+    constructor(name: string, className: string) {
+        this.name = name;
+        this.className = className;
+    }
+
+    greet() : void{
+        console.log(`Hello, my name is ${this.name} and I am in ${this.className}`)
+    }
 }
 
-let userOne : User = {
-    name : "Eloheeka",
-    age : 17,
-    location: "Nyabihu",
-    mentor : false
+const student = new Person("Eloheeka", "Year 2A")
+student.greet();
+
+class Box<T>{
+    contents : T
+    
+
+    constructor(value : T){
+        this.contents = value
+    }
+
+    display(){
+        console.log(`This box contains ${this.contents}`)
+    }
 
 }
 
-let userTwo : User = {
-    name : "Kelly",
-    age : 20,
-    location: "Kigali,Rwanda",
-    mentor : true
+const solid = new Box<string>("books");
+const liquids = new Box<number>(1234);
 
+function WrapInArray<T>(value : T) {
+    return [value];
 }
 
-let userThree : User = {
-    name : "Zion",
-    age : 30,
-    location: "NewYork,USA",
-    mentor : true
-
-}
-
-let userFour : User = {
-    name : "Manzi",
-    age : 188,
-    location: "Toronto,Canada",
-    mentor : false
-
-}
-
-const users : User[] = [userOne, userTwo, userThree];
-
-for(let user of users){
-    console.log(`${user.name} is from ${user.location} and is a ${user.mentor ? 'mentor' : 'learner'}`)
-}
-
-
-const mentors = users.filter(user => user.mentor === true);
-console.log(`Number of mentors : ${mentors.length}`);
-
-const rwandans = users.filter(user => user.location.includes("Rwanda"));
-console.log(`Number of Rwandans : ${rwandans.length}`);
-
-function introduceUser(user: string, age? : number, location :string = "Kigali"){
-    console.log(age ? `${user} is ${age} and lives in ${location}` : `${user} prefers to keep age a secret. and lives in ${location}`)
-}
-
-console.log(introduceUser("Eloheeka",17,"Nyabihu"));
-console.log(introduceUser("Manzi",undefined,"Toronto"   ));
-
-let calc: (x:number, y:number) => number;
- calc = (a,b) => a+b;
- console.log(calc(5,10));
-
- function format(value:string) :string;
- function format(value:number) :number;
- function format(value: number | string) : number | string {
-    return value.toString();
- }
-
- console.log(format("Eloheeka"));
- console.log(format(12345));
-
- let converter : (x: number) => string;
- converter =  (num) => `Value: ${num}`;
- console.log(converter(100));
-
+    type Pet = "Dog" | "Cat" | "Fish";
+    function logsPet  (value: Pet){
+        if( value === "Dog"){
+            console.log("Type is a dog")
+        }
+        if( value === "Cat"){
+            console.log("Type is cat")
+        }
+        if( value === "Fish"){
+            console.log("Type is a Fish");
+        }
+    }
---
+++ main.ts
@@ -0,0 +1,174 @@
+
+interface User {
+    id:number;
+    name: string;
+    email: string;
+    role?: string;
+}
+
+const users: User[] = [
+       {id:1, name:"Alice",email:"m.eloheeka12@gmail.com"},
+       {id:2, name:"John",email:"john10@gmail.com",role:"admin"},
+       {id:3, name:"Max",email:"max@gmail.com",role:"Student"},
+       {id:5, name:"Keza",email:"keza@gmail.com",role:"Teacher"},
+       {id:6, name:"Mico",email:"mico@gmail.com",role:"admin"}
+]
+
+ function getUserInfo(users: User): void {
+    console.log(`${users.name} (id :${users.id}) -email: ${users.email} role: ${users.role ? users.role : "role not assigned"}`)
+ }
+
+//  function listAdmins(users: User[]): void{
+//       const admins =  users.filter(user => user.role === "admin");
+//        for(const admin of admins){
+//         getUserInfo(admin);
+//     }
+    
+// }
+
+// listAdmins(users);
+
+// function getUserByRole(users: User[], role: string) : User[] {
+//     const result = users.filter(user => user.role?.toLowerCase() === role.toLowerCase());
+//     result.forEach(u => getUserInfo(u));
+//     return result;
+// }
+
+// getUserByRole(users, "teacher");
+
+// function getUserByEmail(users:User[],email:string) :User[]{
+//   const result = users.filter(user => user.email?.toLowerCase() === email.toLowerCase());
+//   result.forEach(u => getUserInfo(u));
+//   return result;
+// }
+
+// getUserByEmail(users,"m.eloheeka12@gmail.com")
+ 
+// function getUserByName(users:User[],name:string) :User[]{
+//   const result = users.filter(user => user.name?.toLowerCase() === name.toLowerCase());
+//   result.forEach(u => getUserInfo(u));
+//   return result;
+// }
+
+// getUserByName(users,"mico")
+ 
+// function getUserById(users:User[],id:number) :User[]{
+//   const result = users.filter(user => user.id === id);
+//   result.forEach(u => getUserInfo(u));
+//   return result;
+// }
+
+// getUserById(users,4);
+
+// function searchUsers(users: User[], query:string|number) :User[]{
+//     const querystr = query.toString().toLowerCase();
+
+//     const result = users.filter(user =>
+//         user.name.toLowerCase().includes(querystr) ||
+//         user.email.toLowerCase().includes(querystr) ||
+//         user.id.toString() === querystr ||
+//         user.role?.toLowerCase() === querystr
+//     )
+
+//     result.forEach(u => getUserInfo(u));
+//     return result;
+
+// }
+ 
+// searchUsers(users,"max");
+// searchUsers(users, 5);
+
+function addUser(users: User[], newUser : User): void{
+   if(users.find(user => user.id === newUser.id || user.email.toLowerCase() === newUser.email.toLowerCase())){
+    console.log(`User with id ${newUser.id} and ${newUser.email} already exists.`);
+    return;
+   }else{
+    users.push(newUser);
+    console.log("User added successfully.");
+   }
+} 
+
+addUser(users, {id:7, name:"Sara", email:"saraSasa11@gmail.com", role:"Student"})
+console.log(users);
+
+function updateUser(users: User[], userId:number, update:Partial<User>): void{
+    const userIndex = users.findIndex(user => user.id === userId);
+    if(userIndex === -1){
+        console.log(`User with id ${userId} not found.`);
+    }else{
+        users[userIndex] = {...users[userIndex],...update} as User;
+        console.log(`User with id ${userId} updated successfully.`);
+    }
+}
+
+function updateRole(users: User[], userId:number, newRole:string) :void{
+    const userIndex = users.findIndex(user => user.id === userId);
+    if(userIndex === -1){
+        console.log(`User with id ${userId} not found.`);
+    }else{
+        users[userIndex]!.role = newRole ;
+        console.log(`User with id ${userId} role updated to ${newRole} successfully.`);
+    }
+
+}
+
+class UserManagement{
+    users: User[];
+    currentUser: User | null;
+
+    constructor(users: User[], currentUser:User | null){
+        this.users = users;
+        this.currentUser = currentUser;
+    }
+
+    addUser(newUser: User): void{
+      if(!this.currentUser || this.currentUser.role?.toLowerCase() !== "admin"){
+        console.log("Only admin users can add new users.");
+        return;
+      }
+      if(this.users.find(user => user.id === newUser.id || user.email.toLowerCase() === newUser.email.toLowerCase())){
+         console.log(`User with id ${newUser.id} and ${newUser.email} already exists.`);
+         return;
+   }else{
+    users.push(newUser);
+    console.log("User added successfully.");
+   }
+ };
+   
+ updateUser(userId:number, update:Partial<User>){
+
+    const userIndex = this.users.findIndex(user => user.id === userId);
+    if(userIndex === -1){
+        console.log(`User with id ${userId} not found.`);
+    }else{
+        this.users[userIndex] = {...users[userIndex],...update} as User;
+        console.log(`User with id ${userId} updated successfully.`);
+    }
+
+    };
+    getUserById(userId:number): User | null{
+       const user = this.users.find(u => u.id === userId) || null;
+       return user;
+    }
+    listUsers(): void{
+
+    }
+   searchUsers(query: string | number): User[] {
+    const queryStr = query.toString().toLowerCase();
+
+    const result = this.users.filter(user =>
+        user.name.toLowerCase().includes(queryStr) ||
+        user.email.toLowerCase().includes(queryStr) ||
+        user.id.toString() === queryStr ||
+        user.role?.toLowerCase() === queryStr
+    );
+
+    result.forEach(u => this.getUserInfo(u));
+    return result;
+}
+getUserInfo(user: User): void {
+    console.log(`${user.name} (id: ${user.id}) - email: ${user.email} role: ${user.role ? user.role : "role not assigned"}`);
+}
+
+
+}
